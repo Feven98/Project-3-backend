6e5b5c72f13dc7be37ee9b1a5ccd9eb8d3b404d3
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-01-09 16:25:09 -0500

    add register token

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -13,11 +13,22 @@ try{
     const salt = await bcrypt.genSalt(10)
 
     const passwordHash = await bcrypt.hash(req.body.password, salt)
-    req.body.password = passwordHash
+    const rawPWStore = req.body.password
+
+    req.body.password =  passwordHash
 
     const newUser = await Home.create(req.body)
+    if(newUser){
+        req.body.password = rawPWStore
+        const authenticatedUserToken = createUserToken(req, newUser);
+    res.status(201).json({
+        home: newUser, 
+        isLoggedIn: true, 
+        token: authenticatedUserToken })
+    } else{
+        res.status(400).json({error: "Something went wrong"})
+    }
 
-    res.status(201).json({user: newUser, isLoggedIn: true})
 }catch(err){
     res.status(400).json({err: err.message})
 }
@@ -32,7 +43,7 @@ router.post("/login", async (req, res, next) => {
       const foundUser = await Home.findOne({ username: loggingUser });
       const token = await createUserToken(req, foundUser);
       res.status(200).json({
-        user: foundUser,
+        home: foundUser,
         isLoggedIn: true,
         token,
       });
